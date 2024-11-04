91bd4fcdb17c1f4596e4cf9953e103585c972f6d
Sophie Haber <SHaber@lcs.on.ca>
2024-11-04 14:53:33 -0500

    Initial Commit

---
+++ ChartsExamples/ChartsExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsExamplesApp.swift
+//  ChartsExamples
+//
+//  Created by Sophie Haber on 04.11.24.
+//
+
+import SwiftUI
+
+@main
+struct ChartsExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
