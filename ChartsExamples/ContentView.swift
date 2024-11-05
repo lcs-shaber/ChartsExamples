7e65b3889a584a4dce12326d49f6d9ae6a528971
Sophie Haber <SHaber@lcs.on.ca>
2024-11-04 20:49:56 -0500

    I fixed the development team and added the code for a bar chart

--- ChartsExamples/ContentView.swift
+++ ChartsExamples/ContentView.swift
@@ -6,16 +6,22 @@
 //
 
 import SwiftUI
+import Charts
 
 struct ContentView: View {
+    let weekdays = Calendar.current.shortWeekdaySymbols
+    let steps = [ 10531, 6019, 7200, 8311, 7403, 6503, 9230 ]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Chart {
+            ForEach(weekdays.indices, id: \.self) { index in
+                BarMark(x: .value("Day", weekdays[index]), y: .value("Steps", steps[index]))
+                        .foregroundStyle(by: .value("Day", weekdays[index]))
+                          .annotation {
+                          Text("\(steps[index])")
+                                        }
+               }
         }
-        .padding()
     }
 }
 
