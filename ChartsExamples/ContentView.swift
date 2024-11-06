9eaa4bb9515b4344b58a3fdafb027c9a74a75801
Sophie Haber <SHaber@lcs.on.ca>
2024-11-06 16:45:44 -0500

    I created a different type of bar chart

--- ChartsExamples/ContentView.swift
+++ ChartsExamples/ContentView.swift
@@ -25,6 +25,39 @@ struct ContentView: View {
     }
 }
 
+
+struct BarChartsExamples: View {
+    private var coffeeSales = [
+        (name: "Americano", count: 120),
+        (name: "Cappuccino", count: 234),
+        (name: "Espresso", count: 62),
+        (name: "Latte", count: 625),
+        (name: "Mocha", count: 320),
+        (name: "Affogato", count: 50)
+    ]
+
+    var body: some View {
+        VStack {
+            Chart {
+                ForEach(coffeeSales, id: \.name) { coffee in
+                    BarMark(
+                        x: .value("Cup", coffee.count),
+                        stacking: .normalized
+                       
+                    )
+                    .foregroundStyle(by: .value("Type", coffee.name))
+                }
+            }
+            .frame(height: 100)
+        }
+        .padding()
+    }
+}
+
+
 #Preview {
     ContentView()
+}
+#Preview {
+    BarChartsExamples()
 }
