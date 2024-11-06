4a8700a7675873498d5f1a18b5147c4d43c370e0
Sophie Haber <SHaber@lcs.on.ca>
2024-11-06 16:48:13 -0500

    I moved the multi line line chart to the other line chart, so that they are in the same view

--- ChartsExamples/SimpleLineChartView.swift
+++ ChartsExamples/SimpleLineChartView.swift
@@ -46,7 +46,52 @@ struct SimpleLineChartView: View {
             }
     }
 
+struct MultiLineChartExample: View {
+    
+    let chartData = [ (city: "Hong Kong", data: hkWeatherData),
+                      (city: "London", data: londonWeatherData),
+                      (city: "Taipei", data: taipeiWeatherData) ]
+    
+    var body: some View {
+        
+        Chart {
+            ForEach(chartData, id: \.city) { series in
+                ForEach(series.data) { item in
+                    LineMark(
+                        x: .value("Month", item.date),
+                        y: .value("Temp", item.temperature)
+                    )
+                }
+                .foregroundStyle(by: .value("City", series.city))
+                .symbol(by: .value("City", series.city))
+            }
+        }
+        .frame(height: 300)
+        
+        .chartXAxis {
+        AxisMarks(values: .stride(by: .month)) { value in
+            AxisGridLine()
+            AxisValueLabel(format: .dateTime.month(.defaultDigits))
+        }
+    }
+
+            .chartYAxis {
+        AxisMarks(position: .leading)
+                    }
+                    
+            .chartPlotStyle { plotArea in
+        plotArea
+            .background(.blue.opacity(0.1))
+                }
+            
+                    
+            }
+    }
 
 #Preview {
     SimpleLineChartView()
+}
+
+#Preview {
+    MultiLineChartExample()
 }
